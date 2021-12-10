caf47c6d56e2fdd299968dd594e5afc13cbc0347
Corrie Butler <corriebutler12@gmail.com>
2021-12-10 16:35:18 -0700

    async for the viewalldepartments in server.js

--- server.js
+++ server.js
@@ -2,3 +2,9 @@ const inquirer = require('inquirer');
 const db = require('./db');
 require('console.table');
 
+async function viewAllDepartments() {
+    const [department] = await db.viewAllDepartments();
+    console.table(department);
+};
+
+viewAllDepartments();
