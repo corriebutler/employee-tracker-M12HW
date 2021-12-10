1ba127c13ec634db45ff1229e232ac66e35cb943
Corrie Butler <corriebutler12@gmail.com>
2021-12-10 16:54:02 -0700

    added a main menu

--- server.js
+++ server.js
@@ -2,13 +2,38 @@ const inquirer = require('inquirer');
 const db = require('./db');
 require('console.table');
 
+function mainMenu() {
+    inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'menu',
+            message: 'What would you like to do next?',
+            choices: [
+                'View All Departments',
+                'View All Roles'
+            ]
+        }
+    ])
+    .then(res => {
+        switch(res.menu) {
+            case 'View All Departments':
+                return viewAllDepartments();    
+            case 'View All Roles':
+                return viewAllRoles();
+        }
+    });
+};
+
 async function viewAllDepartments() {
     const [department] = await db.viewAllDepartments();
     console.table(department);
+    mainMenu();
 };
 
 async function viewAllRoles() {
     const [roles] = await db.viewAllRoles();
     console.table(roles);
+    mainMenu();
 };
 
+mainMenu();
