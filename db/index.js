61e0c46c18ba3280f885a8c56952e081370e95dd
Corrie Butler <corriebutler12@gmail.com>
2021-12-10 16:29:43 -0700

    added a call to view all departments

--- db/index.js
+++ db/index.js
@@ -1,1 +1,23 @@
 const connection = require('./connection.js');
+
+class DB {
+    constructor(connection) {
+        this.connection = (connection)
+    }
+
+    viewAllDepartments() {
+        return this.connection.promise().query( 
+            `SELECT 
+                department.id, 
+                department.name
+            FROM
+                department`
+
+        )
+
+    }
+
+
+};
+
+module.exports = new DB(connection);
